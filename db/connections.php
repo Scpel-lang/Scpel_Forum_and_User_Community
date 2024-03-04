4e86e8b698f292ccecfb5b17a17bf2730b61780d
Fidele K <itfidele@gmail.com>
2024-03-04 23:10:38 +0300

    add gitignore

--- db/connections.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $db = mysqli_connect('localhost','phrunsys_scpel_community','1s2c3p4e5l','phrunsys_scpel_community')or  die ('Database connection failed');
-
-
-   // $db = mysqli_connect('localhost','root','admin@123','phrunsys_scpel_community')or  die ('Database connection failed');
-
-?>
