4ce22661be744f57cfb4d813a9d7740a67f353a4
dianajoan <dianajoanita900@gmail.com>
2024-04-06 23:21:06 +0300

    implemented a logout functionality

--- index.php
+++ index.php
@@ -10,6 +10,13 @@ if (!isset($_SESSION['username'])) {
     exit();
 }
 
+// Logout functionality
+if (isset($_POST['logout'])) {
+    session_destroy(); // Destroy session data
+    header("Location: login.php"); // Redirect to login page after logout
+    exit();
+}
+
 // Function to fetch latest discussions
 function fetchLatestDiscussions($db) {
     $query = mysqli_query($db, "SELECT * FROM scpel_forum ORDER BY ID DESC");
@@ -73,6 +80,11 @@ if (!$db) {
                 <li><a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a></li>
                 <li><a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a></li>
                 <li><a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a></li>
+                <li>
+                    <form method="post">
+                        <button type="submit" name="logout" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</button>
+                    </form>
+                </li>
             </ul>
         </div>
     </div>
