80cbf87e7a2a2d76e51cfb1c791c24c9a48fa821
dianajoan <dianajoanita900@gmail.com>
2024-04-07 06:33:52 +0300

    linked the index file

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="index.php" class="flex items-center space-x-3 rtl:space-x-reverse">
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Scpel</span>
         </a>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
